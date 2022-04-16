ba9e3577ec1156c7218976561c891ad7a0f46b57
Putu Audi Pasuatmadi <audipasuatmadi@gmail.com>
2022-04-16 15:29:04 +0800

    [feature] add application modes

--- main.go
+++ main.go
@@ -1,6 +1,7 @@
 package main
 
 import (
+	"flag"
 	"os"
 	"os/signal"
 	"time"
@@ -13,7 +14,17 @@ import (
 	"github.com/audi-skripsi/lambda_batch_processor/pkg/util/logutil"
 )
 
+var logInjectorMode *bool
+var logExtractorMode *bool
+
+func init() {
+	logInjectorMode = flag.Bool("log-injector", false, "used for ignesting logs from kafka to data lake")
+	logExtractorMode = flag.Bool("log-extractor", false, "used for extracting logs from data lake to further processing")
+}
+
 func main() {
+	flag.Parse()
+
 	config.Init()
 	config := config.Get()
 
@@ -46,14 +57,20 @@ func main() {
 		Config:     config,
 	})
 
-	consumer := consumer.NewConsumer(consumer.NewConsumerParams{
-		Logger:        logger,
-		KafkaConsumer: kafkaConsumer,
-		Config:        config,
-		Service:       service,
-	})
-
-	consumer.Init()
+	if *logInjectorMode {
+		logger.Infof("starting app as log-injector mode")
+		consumer := consumer.NewConsumer(consumer.NewConsumerParams{
+			Logger:        logger,
+			KafkaConsumer: kafkaConsumer,
+			Config:        config,
+			Service:       service,
+		})
+		consumer.Init()
+	} else if *logExtractorMode {
+		logger.Infof("starting app as log-extractor mode")
+	} else {
+		logger.Errorf("invalid app mode, should be log-injector or log-extractor")
+	}
 
 	c := make(chan os.Signal, 1)
 	signal.Notify(c, os.Interrupt)
