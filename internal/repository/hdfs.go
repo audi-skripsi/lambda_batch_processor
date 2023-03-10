dc5cc84e621f3489a406290556326ee946c740e4
Putu Audi Pasuatmadi <audipasuatmadi@gmail.com>
2023-03-10 22:51:56 +0800

    [feature] fix concurrency issue on the code

--- internal/repository/hdfs.go
+++ internal/repository/hdfs.go
@@ -16,30 +16,14 @@ func (r *repository) CreateHDFSDirectory(path string) (err error) {
 	return
 }
 
-func (r *repository) WriteToHDFSFile(fileName string, content []byte) (err error) {
-	w, err := r.hdfsClient.CreateFile(fileName, 1, 1048576*64, 0777)
-	if err != nil {
-		r.logger.Errorf("error getting writer to write file: %+v", err)
-		return
-	}
-	defer w.Close()
-
-	_, err = w.Write(content)
-	_, err = w.Write([]byte("\n"))
-	if err != nil {
-		r.logger.Errorf("error writing to file: %+v", err)
-	}
-	return
-}
-
 func (r *repository) AppendToHDFSFile(fileName string, content []byte) (err error) {
 	w, err := r.hdfsClient.Append(fileName)
 	if err != nil {
-		err = r.WriteToHDFSFile(fileName, content)
+		w, err = r.hdfsClient.Create(fileName)
 		if err != nil {
-			r.logger.Errorf("error getting writer to append file: %+v", err)
+			r.logger.Errorf("error writing and appending: %+v", err)
+			return
 		}
-		return
 	}
 	defer w.Close()
 
